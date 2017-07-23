58dd7d3321e363221ac6f7a71615759ec2ad83e6
Amin Hijaz <aminhijaz2001@gmail.com>
2017-07-23 09:07:47 +0300

    register page is ready

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 from flask import Flask , render_template , request
 import dataset
+db = dataset.connect("postgres://axhifbkpwevbyr:fde9124d2d41e4d1b127cd0795358912284f8b2d3d17548297914157fdc703bb@ec2-54-204-32-145.compute-1.amazonaws.com:5432/dd4f0ebt1ijhbf")
 app=Flask(__name__)
-@app.route('/register')
-def site():
-	form=request.form
-	email = form["email"]
-	firstname = form["firstname"]
-	password = form["password"]
-	
+@app.route("/register" , methods=["POST" , "GET"])
+def register():
+	if request.method == "GET":
+		return render_template("register.html")
+	else:
+		form=request.form
+		email = form["email"]
+		username = form["username"]
+		password = form["password"]
+		usersTable = db["users"]
+		entry = {"email" : email ,"username":username , "password":password}
+		if list(usersTable.find_one(username=username))==[]:
+			usersTable.insert(entry)
+			return render_template("register.html" , email=email ,username=username , password=password)
+		else:
+			return "error user is already taken"
+@app.route("/login" , methods=["GET" , "POST"])
+def login():
+	if request.method == "GET":
+		return render_template("login.html")
+	else :
+		username = form["username"]
+		password = form["password"]
+@app.route("/datatableamin7772001")
+def showAll():
+	users=db["users"]
+	allUsers =list(users.all())
+	return render_template("showdatabase.html" , users =allUsers)
 if __name__ == "__main__":
 	app.run()
